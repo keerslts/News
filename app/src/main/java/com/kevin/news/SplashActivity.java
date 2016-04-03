a4a1fe0c00ca76559ec0ae03371144d62d6c5ce3
keerslts <1530161972@qq.com>
2016-04-03 19:32:16 +0800

    splash1 0403

--- app/src/main/java/com/kevin/news/SplashActivity.java
+++ app/src/main/java/com/kevin/news/SplashActivity.java
@@ -2,6 +2,11 @@ package com.kevin.news;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +14,39 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.SplashActivity);
+
+        startAnim();
+    }
+
+    /**
+     * 设置闪屏动画效果
+     * 旋转+放大+透明
+     */
+    private void startAnim() {
+
+        //把两个效果合在一起
+        AnimationSet set = new AnimationSet(false);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        //设置时间
+        rotateAnimation.setDuration(2000);
+        //保持动画状态
+        rotateAnimation.setFillAfter(true);
+
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0, 1, 0, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(2000);
+        scaleAnimation.setFillAfter(true);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+        alphaAnimation.setDuration(2000);
+        alphaAnimation.setFillAfter(true);
+
+        set.addAnimation(rotateAnimation);
+        set.addAnimation(scaleAnimation);
+        set.addAnimation(alphaAnimation);
     }
 }
