3b4cbaf9fe45295d9e6a5cf4f6968873347f0c2e
keerslts <1530161972@qq.com>
2016-04-05 14:07:57 +0800

    0405  恻划页面搭建  SlidingMenu-master 第一次用stadio 导入成功

--- app/src/main/java/com/kevin/news/activity/MainActivity.java
+++ app/src/main/java/com/kevin/news/activity/MainActivity.java
@@ -1,20 +1,31 @@
 package com.kevin.news.activity;
 
-import android.app.Activity;
+
 import android.os.Bundle;
 import android.view.Window;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.kevin.news.R;
 
 /**
  * Created by Kevin on 2016/4/4.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
+
+    private static final String FRAGMENT_LEFT_MENU = "fragment_left_menu";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        setBehindContentView(R.layout.left_menu);// 设置侧边栏
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setBehindOffset(300);
+
+
     }
 }
